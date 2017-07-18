c272ec7555568fa1950662d6100c710abaa82232
Daniel Sitek <dan.sitek@gmail.com>
2017-07-18 15:56:35 +0200

    Added source files

---
+++ src/Avatar.php
@@ -0,0 +1,91 @@
+<?php
+
+/**
+ * Siplified interface using Loader
+ *
+ * @author Daniel Sitek <dan.sitek@gmail.com>
+ */
+
+namespace App;
+
+class Avatar {
+
+    /** @var class  $loader  Loader app class */
+    private $loader;
+
+    private $image_root = null;
+    private $image_array = null;
+
+
+    public function init()
+    {
+        $this->loader = new Avatar\Loader();
+
+        $this->loader
+            ->set_images_root_path( $this->image_root )
+            ->set_images_array( $this->image_array );
+
+        return $this;
+    }
+
+
+    public function set_image_root($path)
+    {
+        $this->image_root = $path;
+
+        return $this;
+    }
+
+
+    public function set_images_array($images)
+    {
+        $this->image_array = $images;
+
+        return $this;
+    }
+
+
+    public function get_categories()
+    {
+        $cat = array();
+
+        foreach ($this->image_array as $key => $value) {
+            array_push($cat, $key);
+        }
+
+        return $cat;
+    }
+
+
+    /**
+     * Get random avatar image
+     */
+    public function random()
+    {
+        return $this->loader
+            ->set_gender()
+            ->load_image();
+    }
+
+
+    /**
+     * Get random man avatar image
+     */
+    public function man()
+    {
+        return $this->loader
+            ->set_gender( 'man' )
+            ->load_image();
+    }
+
+
+    /**
+     * Get random woman avatar image
+     */
+    public function woman()
+    {
+        return $this->loader
+            ->set_gender( 'woman' )
+            ->load_image();
+    }
+}
---
+++ src/Router.php
@@ -0,0 +1,106 @@
+<?php
+
+/**
+ * Setup url routes for Avatar app.
+ *
+ * @author Daniel Sitek <dan.sitek@gmail.com>
+ */
+
+namespace App;
+
+use Silex;
+
+class Router
+{
+
+    /** @var class  $app  Silex app class */
+    private $app;
+
+    /** @var class  $avatar  Avatar app class */
+    private $avatar;
+
+
+    /**
+     * Construct Silex and Avatar app constructor
+     */
+    public function __construct($avatar_instance = null)
+    {
+        $this->app = new Silex\Application();
+        $this->app[ 'debug' ] = true;
+
+        if ($avatar_instance) {
+            $this->avatar = $avatar_instance;
+        } else {
+            $this->avatar = new Avatar();
+        }
+    }
+
+    /**
+     * Set using routes over https
+     */
+    public function use_https()
+    {
+        $this->app['controllers']->requireHttps();
+
+        return $this;
+    }
+
+    /**
+     * Run Silex app
+     */
+    public function run()
+    {
+
+        $this->app->get( '/', function() {
+            $this->stream( $this->get_random_gender() );
+        } );
+
+        $this->app->get( '/{gender}', function( $gender ) {
+            $this->stream( $this->get_defined_gender( $gender ) );
+        } );
+
+        $this->app->run();
+    }
+
+    /**
+     * Render accepted image
+     * @param  array  $data [description]
+     */
+    private function stream( $data = array() )
+    {
+        foreach ($data['headers'] as $key => $value) {
+            header("{$key}: {$value}");
+        }
+
+        return readfile($data['file']);
+    }
+
+    /**
+     * Get random avatar image
+     */
+    private function get_random_gender()
+    {
+        return $this->avatar
+                    ->init()
+                    ->random();
+    }
+
+    /**
+     * Get avatar image specified by gender
+     *
+     * @param  $gender  gender name
+     */
+    private function get_defined_gender($gender)
+    {
+        if ( isset( $gender ) && in_array( $gender, $this->avatar->get_categories() ) ) {
+
+            return $this->avatar
+                        ->init()
+                        ->{$gender}();
+
+        } else {
+
+            $this->app->abort( 404, "There is no picture for the  \"$gender\" category." );
+        }
+    }
+}
