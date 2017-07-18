272f693564e0252750ff10ce7f7d7fe0be41b951
Daniel Sitek <dan.sitek@gmail.com>
2017-07-18 15:57:27 +0200

    Added working example

---
+++ app.php
@@ -0,0 +1,36 @@
+<?php
+
+header('Access-Control-Allow-Origin: *');
+header('Access-Control-Allow-Methods: GET');
+
+require_once __DIR__ . '/vendor/autoload.php';
+
+$avatar = new App\Avatar();
+$router = new App\Router($avatar);
+
+$avatar->set_image_root( __DIR__ . '/images' );
+$avatar->set_images_array( array(
+    'man' => array(
+        '/man/001.png',
+        '/man/002.png',
+        '/man/003.png',
+        '/man/004.png',
+        '/man/005.png'
+    ),
+    'woman' => array(
+        '/woman/001.png',
+        '/woman/002.png',
+        '/woman/003.png',
+        '/woman/004.png',
+        '/woman/005.png'
+    )
+) );
+
+if ( in_array( @$_SERVER['REMOTE_ADDR'], ['127.0.0.1', 'fe80::1', '::1'] ) ) {
+
+    $router->run();
+
+} else {
+
+    $router->use_https()->run();
+}
